ce3664b49a259653092b907d11fde833c68f5a17
James Denton <sgtbelly@gmail.com>
2018-10-20 13:52:00 -0700

    added products and catergories

---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const superagent = require('superagent');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const API = process.env.API_URL || 'http://localhost:3000';
+
+// set up EJS as our template engine
+app.set('view engine', 'ejs');
+
+// Set the public folder up for /...
+app.use( express.static('./public') );
+
+app.get('/', homePage);
+app.get('/categories', catPage);
+app.get('/categories/:name', proPage);
+
+function homePage(request,response) {
+  response.render('site', {page:'./pages/index', title:'Our Site: Home'});
+}
+
+function catPage(request,response) {
+  superagent.get(`${API}/categories`)
+    .then( data => {
+      response.render('site', {categories:data.body, page:'./pages/categories', title:'Our Site: Categories'});
+
+    })
+    .catch( error => console.error(error) );
+
+}
+
+function proPage(request, response) {
+  superagent.get(`${API}/products?category=${request.params.name}`)
+    .then( data => {
+      response.render('site', {products:data.body, page:'./pages/products', title:'Our Site: Products'});
+
+    })
+    .catch( error => console.error(error) );
+
+
+
+
+}
+
+app.listen(PORT, () => console.log(`Server up on ${PORT}`) );
