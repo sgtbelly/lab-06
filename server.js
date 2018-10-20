475b33b95d21cb7f4302a52168c4dfad48140bf5
Matto <matt.mcquain@gmail.com>
2018-10-20 16:28:09 -0700

    edits to DB and server.js

--- server.js
+++ server.js
@@ -46,3 +46,11 @@ function proPage(request, response) {
 }
 
 app.listen(PORT, () => console.log(`Server up on ${PORT}`) );
+
+module.exports = {
+  server: app,
+  start: port => {
+    let PORT = port || process.env.PORT || 8080;
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  },
+};
